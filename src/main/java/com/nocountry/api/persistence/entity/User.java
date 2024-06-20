15f3049a24ab44ea54ddbfeacb932d283c696425
Gonzalo Bolognese <gonza.1703@gmail.com>
2024-06-20 04:44:59 -0300

    Implementación Spring Security, creada y configurada la estrategia de autentificación

---
+++ src/main/java/com/nocountry/api/persistence/entity/User.java
@@ -0,0 +1,98 @@
+package com.nocountry.api.persistence.entity;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "\"user\"")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String email;
+    
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getName (){
+        return name;
+    }
+
+    public String getEmail (){
+        return email;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+
+}
---
+++ src/main/java/com/nocountry/api/persistence/util/RolePermission.java
@@ -0,0 +1,5 @@
+package com.nocountry.api.persistence.util;
+
+public enum RolePermission {
+
+}
